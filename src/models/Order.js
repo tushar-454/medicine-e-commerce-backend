d8654ce05ba6cddc2a55daf757853a7ae04318b0
tushar-454 <imtushar454@gmail.com>
2024-08-10 13:39:20 +0600

    feat: Add Order model with required fields and default values

---
+++ src/models/Order.js
@@ -0,0 +1,109 @@
+const { model, schema } = require('mongoose');
+
+const orderSchema = schema({
+  user: {
+    type: schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  products: [
+    {
+      product: {
+        type: schema.Types.ObjectId,
+        ref: 'Product',
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        required: true,
+      },
+      varient: {
+        name: {
+          type: String,
+          required: true,
+        },
+        price: {
+          type: Number,
+          required: true,
+        },
+      },
+      total: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
+  totalAmount: {
+    type: Number,
+    required: true,
+  },
+  paymentMethod: {
+    type: String,
+    enum: ['cash', 'card', 'bKash', 'rocket'],
+    required: true,
+  },
+  paymentStatus: {
+    type: String,
+    enum: ['paid', 'unpaid', 'pending'],
+    default: 'pending',
+  },
+  orderStatus: {
+    type: String,
+    enum: ['pending', 'processing', 'shipped', 'delivered'],
+    default: 'pending',
+  },
+  shippingAddress: {
+    type: String,
+    required: true,
+  },
+  shippingDivision: {
+    type: String,
+    required: true,
+  },
+  shippingDistrict: {
+    type: String,
+    required: true,
+  },
+  shippingUpazila: {
+    type: String,
+    required: true,
+  },
+  shippingPhone: {
+    type: String,
+    required: true,
+  },
+  shippingEmail: {
+    type: String,
+    required: true,
+  },
+  isPaid: {
+    type: Boolean,
+    default: false,
+  },
+  isDelivered: {
+    type: Boolean,
+    default: false,
+  },
+  isCanceled: {
+    type: Boolean,
+    default: false,
+  },
+  isRefunded: {
+    type: Boolean,
+    default: false,
+  },
+  isReturned: {
+    type: Boolean,
+    default: false,
+  },
+  isExchange: {
+    type: Boolean,
+    default: false,
+  },
+  isDeleted: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+module.exports = model('Order', orderSchema);
