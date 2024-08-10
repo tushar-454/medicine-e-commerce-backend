e1e474250ddf760cdc442c72ff9abad7551f4fd3
tushar-454 <imtushar454@gmail.com>
2024-08-10 17:03:14 +0600

    feat: Update user controller to allow updating user information

--- src/controllers/v1/user.js
+++ src/controllers/v1/user.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcrypt');
+const User = require('../../models/User');
 const { getUserByProperty, createUser } = require('../../services/v1/user');
 
 const createNewUser = async (req, res, next) => {
@@ -42,11 +44,33 @@ const createNewUser = async (req, res, next) => {
 const getUserByEmail = async (req, res, next) => {
   try {
     const { email } = req.params;
+    const user = await User.find({ email }).select({ password: 0 });
+    if (!user) {
+      return res.status(404).json({ status: 404, error: 'User not found' });
+    }
+    return res.status(200).json({ status: 200, user });
+  } catch (error) {
+    next(error);
+  }
+  return null;
+};
+
+const updateUserByEmail = async (req, res, next) => {
+  try {
+    const { email } = req.params;
+    const { name, password, photo, division, district, upazila, address } = req.body;
+
     const user = await getUserByProperty('email', email, 'User');
     if (!user) {
       return res.status(404).json({ status: 404, error: 'User not found' });
     }
-    const userWithoutPassword = user.toObject();
+    const hashPassword = await bcrypt.hash(password, 10);
+    const updatedUser = await User.findOneAndUpdate(
+      { email },
+      { name, password: hashPassword, photo, division, district, upazila, address },
+      { new: true },
+    );
+    const userWithoutPassword = updatedUser.toObject();
     delete userWithoutPassword.password;
     return res.status(200).json({ status: 200, user: userWithoutPassword });
   } catch (error) {
@@ -55,4 +79,4 @@ const getUserByEmail = async (req, res, next) => {
   return null;
 };
 
-module.exports = { createNewUser, getUserByEmail };
+module.exports = { createNewUser, getUserByEmail, updateUserByEmail };
--- src/routes/v1/user.js
+++ src/routes/v1/user.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
-const { createNewUser, getUserByEmail } = require('../../controllers/v1/user');
+const { createNewUser, getUserByEmail, updateUserByEmail } = require('../../controllers/v1/user');
 
 router.post('/create', createNewUser);
 router.get('/:email', getUserByEmail);
+router.put('/:email', updateUserByEmail);
 
 module.exports = router;
