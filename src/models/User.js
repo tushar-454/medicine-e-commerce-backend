ffffca698a6f69270ce5e1dc526bf91eb7c2646f
tushar-454 <imtushar454@gmail.com>
2024-08-10 12:52:19 +0600

    feat: Add User model with required fields and default values

---
+++ src/models/User.js
@@ -0,0 +1,46 @@
+const { model, schema } = require('mongoose');
+
+const userSchema = schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  photo: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ['user', 'admin', 'superadmin'],
+    default: 'user',
+  },
+  division: {
+    type: String,
+    default: 'division',
+  },
+  distric: {
+    type: String,
+    default: 'district',
+  },
+  upazila: {
+    type: String,
+    default: 'upazila',
+  },
+  address: {
+    type: String,
+    default: 'address',
+  },
+});
+
+module.exports = model('User', userSchema);
