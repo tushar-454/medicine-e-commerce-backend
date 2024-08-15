3641adfeb0da0549fd68e9d1f3d4b20cd94852d4
tushar-454 <imtushar454@gmail.com>
2024-08-15 22:57:44 +0600

    add public static image upload code on local branch

--- src/config/multer.js
+++ src/config/multer.js
@@ -1,34 +1,34 @@
-// const multer = require('multer');
-// const path = require('path');
+const multer = require('multer');
+const path = require('path');
 
-// // Set storage engine
-// const storage = multer.diskStorage({
-//   destination: './public/',
-//   filename(req, file, cb) {
-//     cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`);
-//   },
-// });
+// Set storage engine
+const storage = multer.diskStorage({
+  destination: './public/',
+  filename(req, file, cb) {
+    cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`);
+  },
+});
 
-// // Check File Type
-// function checkFileType(file, cb) {
-//   const filetypes = /jpeg|jpg|png/;
-//   const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-//   const mimetype = filetypes.test(file.mimetype);
+// Check File Type
+function checkFileType(file, cb) {
+  const filetypes = /jpeg|jpg|png/;
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = filetypes.test(file.mimetype);
 
-//   if (mimetype && extname) {
-//     return cb(null, true);
-//   }
-//   cb('Error: Images Only!');
-//   return null;
-// }
+  if (mimetype && extname) {
+    return cb(null, true);
+  }
+  cb('Error: Images Only!');
+  return null;
+}
 
-// // Initialize upload variable
-// const upload = multer({
-//   storage,
-//   limits: { fileSize: 1000000 },
-//   fileFilter(req, file, cb) {
-//     checkFileType(file, cb);
-//   },
-// }).single('photo');
+// Initialize upload variable
+const upload = multer({
+  storage,
+  limits: { fileSize: 1000000 },
+  fileFilter(req, file, cb) {
+    checkFileType(file, cb);
+  },
+}).single('photo');
 
-// module.exports = upload;
+module.exports = upload;
